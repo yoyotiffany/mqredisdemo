dab2bb1d39933dd21bb0dfc4223d11108e6e017d
youqian <1278588570@qq.com>
2019-07-01 14:41:15 +0800

    yq2gen 1 commit

--- src/main/java/com/mqredisdemo/demo/Common/test.java
+++ src/main/java/com/mqredisdemo/demo/Common/test.java
@@ -19,7 +19,7 @@ import java.util.Set;
 public class test {
 
     public static void main(String[] args) {
-//yq2gen
+//yq 1 commit
 //1111
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         // 最大连接数
